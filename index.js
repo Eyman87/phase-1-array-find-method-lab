1c55f861226c8a0ee0c8a5ad7548218d894dbfdd
Eyman Eghterafi <eghterafieyman@gmail.com>
2021-12-28 14:10:28 -0600

    commit tree eb8dca48c5cd6ab8c21c5749fc7bf83057f39c2d

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function superbowlWin(arrayOfRecords){
+// Search through all the records
+// When a record has a result of === "W"
+// Return the year of the result
+
+const results = arrayOfRecords.find(game => game.result === "W")
+// return results.year
+if (results === undefined){
+    return undefined
+} else {
+    return results.year
+}
+}
+
+
+//     return record.year === "2015";
+// }
+// console.log(record.find(superbowlWin).year)
